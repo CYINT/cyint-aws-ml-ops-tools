4cd283b0175b8a4f0bb46fe5106e879965a5f61c
Daniel Fredriksen <dfredriksen@cyint.technology>
2021-11-01 05:14:42 -0400

    feature: added ability to return a feature group
    load_feature_group adds the ability to return a sagemaker feature group
    so that it can be used for ingestion, etc.

--- cyint_aws_ml_ops_tools/glue/feature_store.py
+++ cyint_aws_ml_ops_tools/glue/feature_store.py
@@ -3,6 +3,8 @@ from time import sleep
 
 import boto3
 from botocore.exceptions import ClientError
+from sagemaker.feature_store.feature_group import FeatureGroup
+from sagemaker.session import Session
 
 
 def define_feature_group(
@@ -183,4 +185,26 @@ def is_feature_group_different(
         or response["OnlineStoreConfig"] != online_store_config
         or response["OfflineStoreConfig"] != offline_store_config
         or response["Description"] != description
+    )
+
+
+def load_feature_group(feature_group_name):
+
+    region = boto3.Session().region_name
+    boto_session = boto3.Session(region_name=region)
+
+    sagemaker_client = boto_session.client(service_name="sagemaker", region_name=region)
+
+    featurestore_runtime = boto_session.client(
+        service_name="sagemaker-featurestore-runtime", region_name=region
+    )
+
+    feature_store_session = Session(
+        boto_session=boto_session,
+        sagemaker_client=sagemaker_client,
+        sagemaker_featurestore_runtime_client=featurestore_runtime,
+    )
+
+    return FeatureGroup(
+        name=feature_group_name, sagemaker_session=feature_store_session
     )
